e7e49c9954b87a268ca287d98f68dbb55b881bef
myself54188 <aserg14524>
2024-01-19 21:43:56 +0800

    力扣第222题

--- Leetcode_222/Leetcode_222.java
+++ Leetcode_222/Leetcode_222.java
@@ -1,4 +1,38 @@
 package Leetcode_222;
 
-public class Leetcode_222 {
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int num;
+
+    public int countNodes(TreeNode root) {
+        countNodes1(root);
+        return num;
+    }
+
+    public void countNodes1(TreeNode root) {
+        if (root == null)
+            return;
+        num++;
+        countNodes1(root.left);
+        countNodes1(root.right);
+
+    }
 }
