4fa8c04e6f9c92b2340f603e1c99010f5b436d13
myself54188 <aserg14524>
2024-01-19 15:16:24 +0800

    力扣第141题

--- Leetcode_141/Leetcode_141.java
+++ Leetcode_141/Leetcode_141.java
@@ -1,1 +1,25 @@
 package Leetcode_141;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        int i = 0;
+        while (i < 10000000) {
+            if (head == null) {
+                return false;
+            }
+            i++;
+            head = head.next;
+        }
+        return true;
+    }
+}
