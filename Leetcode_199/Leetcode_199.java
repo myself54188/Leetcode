226b5102fd35375d38d4cf9dd032a29bc669dc57
myself54188 <aserg14524>
2024-01-19 19:57:53 +0800

    力扣第199题

---
+++ Leetcode_199/Leetcode_199.java
@@ -0,0 +1,53 @@
+package Leetcode_199;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> lists = new ArrayList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode tmp = queue.poll();
+                list.add(tmp.val);
+                if (tmp.left != null)
+                    queue.offer(tmp.left);
+                if (tmp.right != null)
+                    queue.offer(tmp.right);
+            }
+            lists.add(list);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (List<Integer> tmp : lists) {
+            list.add(tmp.get(tmp.size() - 1));
+        }
+        return list;
+    }
+}
