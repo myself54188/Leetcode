d6961940ddb900a1e3bc81c938f5a9541821be04
myself54188 <aserg14524>
2024-01-19 14:34:56 +0800

    力扣第一题

--- Leetcode_1/Leetcode_1.java
+++ Leetcode_1/Leetcode_1.java
@@ -1,4 +1,6 @@
-class Solution1 {
+package Leetcode_1;
+
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
         for (int i = 0; i < nums.length; i++) {
