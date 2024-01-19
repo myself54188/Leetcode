be281e5974fa5172096956321ff11bd6f13a908a
myself54188 <aserg14524>
2024-01-19 21:04:05 +0800

    力扣第203题

---
+++ Leetcode_203/Leetcode_203.java
@@ -0,0 +1,35 @@
+package Leetcode_203;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        // 创建一个虚拟头结点
+        ListNode dummyhead = new ListNode();
+        dummyhead.next = head;
+        // 创建指针
+        ListNode tmp = dummyhead;
+        while (tmp.next != null) {
+            if (tmp.next.val == val) {
+                tmp.next = tmp.next.next;
+            } else
+                tmp = tmp.next;
+        }
+        return dummyhead.next;
+    }
+}
