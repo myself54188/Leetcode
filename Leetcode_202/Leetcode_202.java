c025b6d2c488770bec93cfda4cdd106269521f1b
myself54188 <aserg14524>
2024-01-19 20:06:23 +0800

    力扣第202题

---
+++ Leetcode_202/Leetcode_202.java
@@ -0,0 +1,26 @@
+package Leetcode_202;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> record = new HashSet<>();
+        while (n != 1 && !record.contains(n)) {
+            record.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    // 获取 n 的各个位数平方之和
+    private int getNextNumber(int n) {
+        int res = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            res += temp * temp;
+            n = n / 10;
+        }
+        return res;
+    }
+}
