72934bbc0602a2c5261a11ca4c72079a32bf91f2
myself54188 <aserg14524>
2024-01-19 17:19:59 +0800

    力扣第142题

--- Leetcode_142/Leetcode_142.java
+++ Leetcode_142/Leetcode_142.java
@@ -10,8 +10,26 @@ class ListNode {
     }
 }
 
-public class Solution {
+class Solution {
     public ListNode detectCycle(ListNode head) {
+        ListNode p = new ListNode(0); // 快指针
+        ListNode q = new ListNode(0); // 慢指针
+        p = head;
+        q = head;
+        while (p != null && p.next != null) {
+            p = p.next.next;
+            q = q.next;
+            if (p == q) { // 相遇了
+                ListNode index1 = p;
+                ListNode index2 = head;
 
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
     }
 }
