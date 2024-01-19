32b7ec308c55206e92c065b7e1abebaba7af4bca
myself54188 <aserg14524>
2024-01-19 21:17:04 +0800

    力扣第217题

--- Leetcode_217/Leetcode_217.java
+++ Leetcode_217/Leetcode_217.java
@@ -1,4 +1,18 @@
 package Leetcode_217;
 
-public class Leetcode_217 {
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums) {
+            if (map.containsKey(x)) {
+                return true;
+            } else {
+                map.put(x, 1);
+            }
+        }
+        return false;
+    }
 }
