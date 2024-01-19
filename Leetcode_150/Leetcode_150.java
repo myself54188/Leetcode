b14b34ee1a03eafb8e141945ada7042ac38437fc
myself54188 <aserg14524>
2024-01-19 17:38:59 +0800

    力扣第150题

---
+++ Leetcode_150/Leetcode_150.java
@@ -0,0 +1,34 @@
+package Leetcode_150;
+
+
+import java.util.Stack;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        //"2", "1", "+", "3", "*"
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                switch (tokens[i]) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            } else {
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
