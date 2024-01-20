54691127f32d28169f66f90223f2f9d0df2d2b87
myself54188 <aserg14524>
2024-01-20 16:21:12 +0800

    2024.1.20

--- Leetcode_0401_0500/Leetcode_412/Leetcode_412.java
+++ Leetcode_0401_0500/Leetcode_412/Leetcode_412.java
@@ -1,10 +1,22 @@
 package Leetcode_0401_0500.Leetcode_412;
 
+import java.util.ArrayList;
 import java.util.List;
 
-//Leetcode_404
 class Solution {
     public List<String> fizzBuzz(int n) {
-
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(i + "");
+            }
+        }
+        return list;
     }
 }
