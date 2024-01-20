86b822840e18cf3cce603f01a235bb0bbea8d48d
myself54188 <aserg14524>
2024-01-20 14:59:32 +0800

    力扣第349题

---
+++ Leetcode_349/Leetcode_349.java
@@ -0,0 +1,30 @@
+package Leetcode_349;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] arr = new int[10001];
+        for (int i = 0; i < nums1.length; i++) {
+            arr[nums1[i]] = 1;
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (arr[nums2[i]] == 1) {
+                arr[nums2[i]] = 2;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                list.add(i);
+            }
+        }
+        int[] brr = new int[list.size()];
+        for (int i = 0; i < brr.length; i++) {
+            brr[i] = list.get(i);
+        }
+        return brr;
+    }
+}
