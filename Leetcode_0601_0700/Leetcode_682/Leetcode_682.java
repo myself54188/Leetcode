44422c031de9ac9ce0b1e7e039d9de6517524cdb
myself54188 <aserg14524>
2024-01-23 01:34:01 +0800

    2024.1.23.01.33更新

--- Leetcode_0601_0700/Leetcode_682/Leetcode_682.java
+++ Leetcode_0601_0700/Leetcode_682/Leetcode_682.java
@@ -1,7 +1,23 @@
 package Leetcode_0601_0700.Leetcode_682;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int calPoints(String[] operations) {
-
+        List<Integer> list = new ArrayList<>();
+        for (String a : operations) {
+            switch (a) {
+                case "C" -> list.remove(list.size() - 1);
+                case "D" -> list.add(list.get(list.size() - 1) * 2);
+                case "+" -> list.add(list.get(list.size() - 1) + list.get(list.size() - 2));
+                default -> list.add(Integer.parseInt(a));
+            }
+        }
+        int sum = 0;
+        for (Integer in : list) {
+            sum += in;
+        }
+        return sum;
     }
 }
