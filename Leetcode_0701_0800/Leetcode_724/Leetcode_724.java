b00b32d9ff299112a1729b709219ec102622b7c6
myself54188 <aserg14524>
2024-02-24 17:51:00 +0800

    2024.2.24.17.50更新

---
+++ Leetcode_0701_0800/Leetcode_724/Leetcode_724.java
@@ -0,0 +1,17 @@
+package Leetcode_0701_0800.Leetcode_724;
+
+import java.util.Arrays;
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+}
